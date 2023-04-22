f608e060b95d58aa2d16d7aef13c3a6a47f8d08e
Igor Varejão <imsvarejao@gmail.com>
2023-04-22 18:13:54 -0300

    Deleted old vibnet

--- benchmarks/DLDB/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from .MLP import MLP
-from .CNN_1d import CNN as CNN_1d
-from .CNN_2d import CNN as CNN_2d
-from .Alexnet1d import AlexNet as Alexnet1d
-from .Resnet1d import ResNet as Resnet1d
-from .LeNet1d import LeNet as LeNet1d
