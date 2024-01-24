702bd046c4e9299e9ee13383e38c34e813274b68
Luciano Henrique <luciano.h.peixoto@gmail.com>
2024-01-24 14:21:33 -0300

    Add results logging

--- vibnet/cli/sklearn.py
+++ vibnet/cli/sklearn.py
@@ -1,3 +1,4 @@
+import os
 from datetime import datetime
 from pathlib import Path
 
@@ -5,29 +6,32 @@ import pandas as pd
 from rich import print
 from sklearn.model_selection import LeaveOneGroupOut, cross_validate
 
+import wandb
 from vibnet.config import ConfigSklearn
 from vibnet.utils.sklearn import TrainDataset
 
-from .common import group_class, set_deterministic, wandb_login
+from .common import group_class, is_logged, set_deterministic, wandb_login
 
 
 def main(cfg: Path):
     actual_datetime = datetime.now()
-
-    config = ConfigSklearn(cfg)
-    seed = config.get_yaml().get("seed", None)
-    if seed is not None:
-        set_deterministic(seed)
 
     try:
         wandb_login()
     except RuntimeError as e:
         print(f"[bold red]Alert![/bold red] Running without wandb: {e}")
 
+    config = ConfigSklearn(cfg)
+    seed = config.get_yaml().get("seed", None)
+    if seed is not None:
+        set_deterministic(seed)
+    else:
+        print("[bold red]Alert![/bold red] Seed was not set!")
+
     dataset_name = config["dataset"]["name"]
     dataset = config.get_dataset()
     group_obj = group_class(dataset_name)(dataset=dataset, config=config)
-    groups = group_obj.groups()
+    groups = group_obj.groups().reshape(-1)
 
     estimator = config.get_estimator()
     dataset = TrainDataset(dataset)
@@ -38,11 +42,19 @@ def main(cfg: Path):
         dataset.targets,
         groups=groups,
         cv=cv,
-        scoring=["accuracy", "f1_macro"],
+        scoring=["accuracy", "f1_macro", "balanced_accuracy"],
+        verbose=4,
     )
 
     df = pd.DataFrame(results)
+    df = df.rename_axis("fold", axis="index")
     print(df)
     filename = f"results-{actual_datetime.isoformat()}.csv"
     print(f"Saving csv at [bold green]{filename}[/bold green]")
     df.to_csv(filename)
+
+    if is_logged():
+        project_name = os.environ["WANDB_PROJECT"]
+        run = wandb.init(project=project_name, name="Results")
+        table = wandb.Table(data=df)
+        run.log({"Results": table})
