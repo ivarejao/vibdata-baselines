fb9e37fd06089c8b93ba39831984cc7ca94302f2
Igor Varejão <imsvarejao@gmail.com>
2024-01-25 23:27:53 -0300

    Add resampling methods to the `IMS` and `XJTU` datasets
    As these datasets are *run to failure* tests, they are very imbalanced,
    therefore, it was designed resampling strategies for them in
    `lib/data/resampling.py` which is executed in `lib/config.py` when
    loading the dataset defined.

--- vibnet/config.py
+++ vibnet/config.py
@@ -6,7 +6,10 @@ import vibdata.raw as datasets
 import vibdata.deep.signal.transforms as deep_transforms
 from vibdata.deep.DeepDataset import DeepDataset, convertDataset
 
+import vibnet.data.resampling as resampler_pkg
 from vibnet.models.model import Model
+
+RESAMPLING_DATASETS = {"IMS", "XJTU"}
 
 
 class Config:
@@ -105,5 +108,9 @@ class Config:
         transforms = deep_transforms.Sequential([getattr(deep_transforms, t["name"])(**t["parameters"]) for t in transforms_config])
         # Convert the raw dataset to deepdataset
         convertDataset(dataset=raw_dataset, transforms=transforms, dir_path=deep_root_dir)
-        dataset = DeepDataset(deep_root_dir, transforms)
+        dataset = DeepDataset(deep_root_dir)
+        # Resample the dataset if is needed
+        if dataset_name in RESAMPLING_DATASETS:
+            resampler = getattr(resampler_pkg, "Resampler" + dataset_name)()
+            dataset = resampler.resample(dataset)
         return dataset
