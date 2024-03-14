bc7e048fbd8d677bf9bcc9bfad5e9f419880adf6
Lucas Gabriel <lucasgabriiel.oliveira@gmail.com>
2024-03-14 09:36:25 -0300

    Change standardize temporarily

--- vibnet/utils/MemeDataset.py
+++ vibnet/utils/MemeDataset.py
@@ -16,14 +16,15 @@ class MemeDataset(Dataset):
         ret = self.dataset[idx]
         X = ret["signal"]
         X = np.array(X, dtype=float)  # Force the array type
-        if isinstance(ret["metainfo"], pd.Series):
-            y = ret["metainfo"]["label"]
-        else:
-            y = ret["metainfo"]["label"].values.reshape(-1, 1)
-
-        if self.standardize:
-            y -= self.dataset.metainfo["label"].min()
-        y = y.astype("int")  # Force the array type
+        # if isinstance(ret["metainfo"], pd.Series):
+        #     y = ret["metainfo"]["label"]
+        # else:
+        #     y = ret["metainfo"]["label"].values.reshape(-1, 1)
+        #
+        # if self.standardize:
+        #     y -= self.dataset.metainfo["label"].min()
+        # y = y.astype("int")  # Force the array type
+        y = self.targets[idx]
         return X, y
 
     @property
@@ -36,4 +37,10 @@ class MemeDataset(Dataset):
         targets = np.array(self.dataset.metainfo["label"], dtype=np.int64)
         if self.standardize:
             targets -= targets.min()
+        ###
+        unique_labels = np.unique(targets)
+        label_map = {label: idx for idx, label in enumerate(unique_labels)}
+        mapped_labels = np.array([label_map[label] for label in targets])
+        targets = mapped_labels
+        ###
         return targets
