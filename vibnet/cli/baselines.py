ca28d27b1c2a6e3374cc8b7c2816fb8b1a2afbb4
Lucas Gabriel <lucasgabriiel.oliveira@gmail.com>
2024-05-29 12:33:20 -0300

    refactoring baselines.py

--- vibnet/cli/baselines.py
+++ vibnet/cli/baselines.py
@@ -1,19 +1,22 @@
+import csv
+import json
 import os
-from typing import List
 from pathlib import Path
-from dataclasses import dataclass
+from typing import List
 
 import numpy as np
+import pandas as pd
 import wandb
 from dotenv import load_dotenv
-from sklearn.metrics import classification_report, balanced_accuracy_score
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import GridSearchCV, StratifiedKFold, LeaveOneGroupOut, cross_val_predict
+from sklearn.metrics import classification_report, balanced_accuracy_score
+from sklearn.model_selection import GridSearchCV, StratifiedKFold, LeaveOneGroupOut, cross_validate
+from sklearn.neighbors import KNeighborsClassifier
 from vibdata.deep.DeepDataset import DeepDataset
 
 import vibnet.data.group_dataset as groups_module
-from vibnet.config import Config
 from vibnet.cli.common import Split
+from vibnet.config import Config
 from vibnet.data.group_dataset import GroupMirrorBiased
 
 
@@ -25,70 +28,108 @@ def get_features(dataset: DeepDataset) -> (List[int], List[int]):
 
 
 def classifier_biased(cfg: Config, inputs: List[int], labels: List[int], groups: List[int]) -> List[int]:
+    model_name = cfg["model"]["name"]
     seed = cfg["seed"]
-    parameters = cfg["params_grid"]
-
     num_folds = len(set(groups))
 
-    model = RandomForestClassifier(random_state=seed)
+    if model_name == "randomforest":
+        parameters = cfg["params_grid"]
+        cv_inner = StratifiedKFold(n_splits=3, shuffle=True, random_state=seed)
 
-    cv_outer = StratifiedKFold(n_splits=num_folds, shuffle=True, random_state=seed)
-    cv_inner = StratifiedKFold(n_splits=3, shuffle=True, random_state=seed)
+        model = GridSearchCV(
+            estimator=RandomForestClassifier(random_state=seed),
+            param_grid=parameters,
+            scoring="balanced_accuracy",
+            cv=cv_inner,
+            n_jobs=-1,
+        )
 
-    clf = GridSearchCV(
-        estimator=model,
-        param_grid=parameters,
-        scoring="balanced_accuracy",
-        cv=cv_inner,
-        n_jobs=-1,
-    )
+    elif model_name == "knn":
+        n_neighbors = cfg["n_neighbors"]
+        model = KNeighborsClassifier(n_neighbors=n_neighbors)
 
-    y_pred = cross_val_predict(clf, inputs, labels, cv=cv_outer)
+    else:
+        raise Exception(f"Unexpected model_name {model_name}.")
 
-    return y_pred
+    cv = StratifiedKFold(n_splits=num_folds, shuffle=True, random_state=seed)
+    scores = cross_validate(model, inputs, labels, scoring=('accuracy', 'f1_macro', 'balanced_accuracy'), cv=cv)
+
+    return scores
 
 
 def classifier_predefined(cfg: Config, inputs: List[int], labels: List[int], groups: List[int]) -> List[int]:
+    model_name = cfg["model"]["name"]
     seed = cfg["seed"]
-    parameters = cfg["params_grid"]
+    cv = LeaveOneGroupOut()
 
-    model = RandomForestClassifier(random_state=seed)
+    if model_name == "randomforest":
+        parameters = cfg["params_grid"]
+        cv_inner = LeaveOneGroupOut()
 
-    cv_inner = LeaveOneGroupOut()
-    clf = GridSearchCV(
-        estimator=model,
-        param_grid=parameters,
-        scoring="balanced_accuracy",
-        cv=cv_inner,
-        n_jobs=-1,
-    )
+        model = GridSearchCV(
+            estimator=RandomForestClassifier(random_state=seed),
+            param_grid=parameters,
+            scoring="balanced_accuracy",
+            cv=cv_inner,
+            n_jobs=-1,
+        )
 
-    cv_outer = LeaveOneGroupOut()
-    y_pred = cross_val_predict(clf, inputs, labels, groups=groups, cv=cv_outer, fit_params={"groups": groups})
+        scores = cross_validate(model, inputs, labels, groups=groups,
+                                scoring=('accuracy', 'f1_macro', 'balanced_accuracy'),
+                                cv=cv, params={"groups": groups})
 
-    return y_pred
+    elif model_name == "knn":
+        n_neighbors = cfg["n_neighbors"]
+        model = KNeighborsClassifier(n_neighbors=n_neighbors)
+
+        scores = cross_validate(model, inputs, labels, groups=groups,
+                                scoring=('accuracy', 'f1_macro', 'balanced_accuracy'),
+                                cv=cv)
+    else:
+        raise Exception(f"Unexpected model_name {model_name}.")
+
+    return scores
 
 
-def results(dataset: DeepDataset, y_true: List[int], y_pred: List[int]) -> None:
+def results(dataset: DeepDataset, y_true: List[int], y_pred: List[int], split: Split, file_name: str,
+            model_name: str) -> None:
     labels = dataset.get_labels_name()
     labels = [label if label is not np.nan else "NaN" for label in labels]
 
+    balanced_accuracy = balanced_accuracy_score(y_true, y_pred)
+
+    # Show the results in terminal
     print(f"{classification_report(y_true, y_pred, target_names=labels)}")
-    print(f"Balanced accuracy: {balanced_accuracy_score(y_true, y_pred):.2f}")
+    print(f"Balanced accuracy: {balanced_accuracy:.2f}")
+
+    # Save the results in a .json file
+    bias = split.name
+    report = classification_report(y_true, y_pred, target_names=labels, output_dict=True)
+    report['balanced accuracy'] = balanced_accuracy
+    directory = "results_baselines/" + model_name + "/" + bias
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    full_file_name = file_name + ".json"
+    full_path = os.path.join(directory, full_file_name)
+    with open(full_path, 'w') as json_file:
+        json.dump(report, json_file, indent=4)
+    print(f"\nThe file .json was saved in {full_path}.")
 
 
 def configure_wandb(run_name: str, cfg: Config, cfg_path: str, groups: List[int], split: Split) -> None:
     wandb.login(key=os.environ["WANDB_KEY"])
+    config = {
+        "model": cfg["model"]["name"],
+        "folds": len(set(groups)),
+        "split": split.value
+    }
+    if "params_grid" in cfg:
+        config["params_grid"] = cfg["params_grid"]
     wandb.init(
         # Set the project where this run will be logged
         project=os.environ["WANDB_PROJECT"],
         # Track essentials hyperparameters and run metadata
-        config={
-            "model": cfg["model"]["name"],
-            "folds": len(set(groups)),
-            "params_grid": cfg["params_grid"],
-            "split": split.value,
-        },
+        config=config,
         # Set the name of the experiment
         name=run_name,
     )
@@ -96,10 +137,20 @@ def configure_wandb(run_name: str, cfg: Config, cfg_path: str, groups: List[int]
     wandb.save(cfg_path, policy="now")
 
 
+def save_scores(scores, split, model_name, file_name):
+    df_scores = pd.DataFrame(scores)
+
+    bias = split.name
+    filepath = Path("results_baselines/" + model_name + "/" + bias + '/' + file_name + ".csv")
+    filepath.parent.mkdir(parents=True, exist_ok=True)
+    df_scores.to_csv(filepath, index=False, quoting=csv.QUOTE_ALL)
+
+
 def main(cfg: Path, split: Split):
     load_dotenv()
     config = Config(cfg)
 
+    model_name = config["model"]["name"]
     dataset_name = config["dataset"]["name"]
     dataset = config.get_dataset()
 
@@ -107,14 +158,17 @@ def main(cfg: Path, split: Split):
     groups = group_obj.groups()
 
     if split is Split.biased_mirrored:
-        groups = GroupMirrorBiased(dataset=dataset, config=config, custom_name=config["run_name"]).groups(groups)
+        groups = GroupMirrorBiased(
+            dataset=dataset, config=config, custom_name=config["run_name"]
+        ).groups(groups)
 
-    configure_wandb(dataset_name, config, cfg, groups, split)
+    configure_wandb(dataset_name, config, str(cfg), groups, split)
 
     X, y = get_features(dataset)
-    if split is Split.biased_usual:
-        y_pred = classifier_biased(config, X, y, groups)
-    else:
-        y_pred = classifier_predefined(config, X, y, groups)
 
-    results(dataset, y, y_pred)
+    if split is Split.biased_usual:
+        scores = classifier_biased(config, X, y, groups)
+    else:
+        scores = classifier_predefined(config, X, y, groups)
+
+    save_scores(scores, split, model_name, config["run_name"].lower())
