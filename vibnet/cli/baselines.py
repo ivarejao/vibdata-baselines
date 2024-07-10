2dbb2ecda68a4c81d06cc93590dba7d50ebe9401
Igor Varejão <imsvarejao@gmail.com>
2024-07-09 21:34:44 -0300

    unstable version

--- vibnet/cli/baselines.py
+++ vibnet/cli/baselines.py
@@ -1,9 +1,12 @@
+from datetime import datetime
 import os
+import pickle
 from typing import List, Tuple
 from pathlib import Path
 from dataclasses import dataclass
 
 import numpy as np
+import pandas as pd
 import wandb
 from dotenv import load_dotenv
 from sklearn.metrics import classification_report, balanced_accuracy_score
@@ -15,7 +18,6 @@ import vibnet.data.group_dataset as groups_module
 from vibnet.config import ConfigSklearn
 from vibnet.cli.common import Split
 from vibnet.data.group_dataset import GroupMirrorBiased
-
 
 def classifier_biased(cfg: ConfigSklearn, inputs: List[int], labels: List[int], groups: List[int]) -> List[int]:
     seed = cfg["seed"]
@@ -42,6 +44,49 @@ def classifier_predefined(cfg: ConfigSklearn, inputs: List[int], labels: List[in
     y_pred = cross_val_predict(clf, inputs, labels, groups=groups, cv=cv_outer, fit_params=fit_params)
 
     return y_pred
+
+def round_cv(cfg: ConfigSklearn, inputs: List[int], labels: List[int], groups: List[int], metainfo: pd.DataFrame) -> List[int]:
+
+    from vibnet.data.round_cv import NewLogo, RepeteadNewLogo
+    from sklearn.model_selection import cross_validate
+
+    NUM_REPEATS = 30
+    actual_datetime = datetime.now()
+
+    # num_folds_per_dataset = {"CWRU": lambda df: df["load"].nunique(), 
+    #        "MFPT": lambda df: df[["load", "label"]].drop_duplicates().groupby("label").count()["load"].mean(), 
+    #        "PU": lambda df: df[["radial_force_n", "rotation_hz", "load_nm"]].drop_duplicates().shape[0]}
+    # folds_per_round = num_folds_per_dataset[cfg["dataset"]["name"]](metainfo)
+
+    folds_per_round = np.unique(groups).shape[0] / np.unique(labels).shape[0]  # folds_per_round = total_groups / total_labels = num_conditions
+    num_repeats = np.ceil(NUM_REPEATS / folds_per_round).astype(int)
+    print("Num_repeat: ", num_repeats)
+    print("Folds per round: ", folds_per_round)
+
+    cv_outer = RepeteadNewLogo(n_repeats=num_repeats, random_state=cfg["seed"], y=labels, groups=groups)
+    cv_inner = NewLogo(shuffle=True, random_state=cfg["seed"], combinations=cv_outer.combinations)
+
+    clf = cfg.get_estimator(grid_serch_cv=cv_inner)
+
+    fit_params = {"classifier__groups": groups} if "params_grid" in cfg else None
+
+    # Currently, the `cross_val_predict` does not support Cross-Validation with Rounds
+    # https://github.com/scikit-learn/scikit-learn/issues/16135#issue-550525513
+    stats = cross_validate(clf, inputs, labels, groups=groups, cv=cv_outer, fit_params=fit_params, scoring=["f1_macro", "balanced_accuracy"],
+                           return_indices=True, error_score="raise", verbose=2)
+
+    print("Num splits: ", len(stats["indices"]["test"]))
+    # TODO: Change format back to csv
+    filename = f"results-baselines-{cfg.config.get('run_name', None)}-{actual_datetime.isoformat()}.pkl"
+
+    # pd.DataFrame(stats).to_csv(filename, index=False)
+    pickle.dump(stats, open(filename, "wb"))
+
+    print(f"Mean F1 macro: {np.mean(stats['test_f1_macro']):.2f}")
+    print(f"Mean Balanced accuracy: {np.mean(stats['test_balanced_accuracy']):.2f}")
+    print(f"Stats saved in {filename}")
+
+    # return y_pred
 
 
 def results(dataset: DeepDataset, y_true: List[int], y_pred: List[int]) -> None:
@@ -76,22 +121,51 @@ def configure_wandb(run_name: str, cfg: ConfigSklearn, cfg_path: str, groups: Li
 def main(cfg: Path, split: Split):
     load_dotenv()
     config = ConfigSklearn(cfg)
+    # config.clear_cache()
 
     dataset_name = config["dataset"]["name"]
     dataset = config._get_dataset_deep()
 
-    group_obj = getattr(groups_module, "Group" + dataset_name)(dataset=dataset, config=config)
+    # group_obj = getattr(groups_module, "Group" + dataset_name)(dataset=dataset, config=config)
+    # groups = group_obj.groups()
+    from vibnet.data.round_cv import GroupRepeatedCWRU48k
+    import pandas as pd
+    group_obj = GroupRepeatedCWRU48k(dataset=dataset, config=config, custom_name=config["run_name"])
     groups = group_obj.groups()
+    groups_str = groups.copy()  # TODO: remove
+    groups = pd.Categorical(groups).codes
 
-    if split is Split.biased_mirrored:
-        groups = GroupMirrorBiased(dataset=dataset, config=config, custom_name=config["run_name"]).groups(groups)
+    # if split is Split.biased_mirrored:
+    #     groups = GroupMirrorBiased(dataset=dataset, config=config, custom_name=config["run_name"]).groups(groups)
 
-    configure_wandb(config["run_name"], config, cfg, groups, split)
+    # configure_wandb(config["run_name"], config, cfg, groups, split)
 
     X, y = config.get_dataset()
-    if split is Split.biased_usual:
-        y_pred = classifier_biased(config, X, y, groups)
-    else:
-        y_pred = classifier_predefined(config, X, y, groups)
+    # from vibnet.data.round_cv import _compute_combinations
+    # CLASS_DEF = [-1, "I", "R", "O"]
+    # combs = [c for c in _compute_combinations(y, groups)]
+    # print("Total combs: ", len(combs))
+    # for i in range(4):
+    #     print("round: ", i)
+    #     c = combs[i]
+    #     for i in range(4):
+    #         print("fold: ", i, end="-> ")
+    #         for key, item in c.items():
+    #             gp = item[i]
+    #             gp = CLASS_DEF[int(gp.split(" ")[0])] + " " + gp.split(" ")[1]
+    #             print(f"{gp}", end=", ")
+    #         print()
+    #     print()
+    round_cv(config, X, y, groups, metainfo=dataset.get_metainfo())
 
-    results(dataset, y, y_pred)
+
+
+    # data = {"y_pred":y_pred, "y_true":np.concatenate([y,] * num_repeats), "groups":np.concatenate([groups,] * num_repeats)}
+    # data["round"] = np.array([i for i in range(num_repeats) for _ in range(len(y))])
+    # results = pd.DataFrame(data).to_csv("results.csv", index=False)
+    # if split is Split.biased_usual:
+    #     y_pred = classifier_biased(config, X, y, groups)
+    # else:
+    #     y_pred = classifier_predefined(config, X, y, groups)
+    
+    # results(dataset, y, y_pred)
--- vibnet/data/round_cv.py
+++ vibnet/data/round_cv.py
@@ -2,48 +2,111 @@ import itertools
 
 import numpy as np
 from sklearn.model_selection import BaseCrossValidator
-from sklearn.model_selection._split import check_array, _RepeatedSplits, GroupsConsumerMixin
 from sklearn.utils.validation import check_random_state
+from sklearn.model_selection._split import GroupsConsumerMixin, check_array, _RepeatedSplits
 
-class RepeatedLeaveBalancedGroupOut(_RepeatedSplits):
 
-    def __init__(self, n_repeats : int = 1, random_state=None):
-        self.used_combinations = set()
+class RepeteadNewLogo(_RepeatedSplits):
+    def __init__(self, n_repeats: int = 1, random_state=None, y=None, groups=None):
+        self.n_splits = int(len(np.unique(groups)) / len(
+            np.unique(y)
+        ))  # n_splits = total_groups / total_labels = num_conditions
+        self.combinations = self._compute_combinations(y, groups)
         super().__init__(
-            LeaveBalancedGroupsOut, n_repeats=n_repeats, random_state=random_state, used_combinations=self.used_combinations,
+            NewLogo,
+            n_repeats=n_repeats,
+            random_state=random_state,
+            combinations=self.combinations,
+            update_combinations=True,
         )
 
+    def _compute_combinations(self, y, groups):
+        labels = np.unique(y)
+        # Create a dictionary to map labels to their unique groups
+        initial_states = {label: np.unique(groups[y == label]).tolist() for label in labels}
 
-def _compute_combinations(self, y, groups):
-    labels = np.unique(y)
-    # Create a dictionary to map labels to their unique groups
-    initial_states = {label: np.unique(groups[y == label]) for label in labels}
+        def shift_groups(groups, shift):
+            return groups[-shift:] + groups[:-shift]
 
-    def shift_groups(groups, shift):
-        return groups[-shift:] + groups[:-shift]
+        def backtrack(currrent_combination, label_idx):
+            # print(currrent_combination)
+            # print(initial_states)
+            # print(currrent_combination.keys() == initial_states.keys())
+            # print("---")
+            if currrent_combination.keys() == initial_states.keys():
+                yield currrent_combination
+                return 
 
-    def backtrack(currrent_combination, label_idx):
-        if currrent_combination.keys() == initial_states.keys():
-            yield currrent_combination
+            for shift in range(self.n_splits):  
+                label = labels[label_idx]
+                shifted_groups = shift_groups(initial_states[label], shift)
+                currrent_combination[label] = shifted_groups
+                yield from backtrack(currrent_combination.copy(), label_idx + 1)
+                currrent_combination.pop(label)
 
-        for shift in range(self.n_splits):
-            label = labels[label_idx]
-            shifted_groups = shift_groups(initial_states[label], shift)
-            currrent_combination[label] = shifted_groups
-            yield from backtrack(currrent_combination.copy(), label_idx + 1)
-            currrent_combination.pop(label)
-        
-        yield from backtrack([], 0)
+        yield from backtrack({}, 0)
 
-    # Create combinations that ensure each fold has one unique group per label
-    unique_combinati
-    if self.shuffle:
-        check_random_state(self.random_state).shuffle(group_combinations)
-    return group_combinations
+
+class NewLogo(GroupsConsumerMixin, BaseCrossValidator):
+
+    # TODO: Remove this class variable if possible
+    round_combination = None
+
+    def __init__(self, shuffle: bool = False, random_state=None, combinations=None, update_combinations=False):
+        if not isinstance(shuffle, bool):
+            raise TypeError("shuffle must be True or False; got {0}".format(shuffle))
+
+        if not shuffle and random_state is not None:  # None is the default
+            raise ValueError(
+                (
+                    "Setting a random_state has no effect since shuffle is "
+                    "False. You should leave "
+                    "random_state to its default (None), or set shuffle=True."
+                ),
+            )
+
+        if combinations is None:
+            raise ValueError("combinations should not be None.")
+
+        self.shuffle = shuffle
+        self.random_state = random_state
+        if update_combinations:
+            NewLogo.round_combination = next(combinations)
+        self.current_combination = NewLogo.round_combination 
+        # Ensure that the group distribution is consistent
+        lengths = [len(values) for values in self.current_combination.values()]
+        if not all(length == lengths[0] for length in lengths):
+            raise ValueError("All values in groups_distribution should have the same length.")
+        self.n_splits = lengths[0]
+
+    def _iter_test_masks(self, X, y, groups):
+        if groups is None:
+            raise ValueError("The 'groups' parameter should not be None.")
+        # We make a copy of groups to avoid side-effects during iteration
+        groups = check_array(groups, input_name="groups", copy=True, ensure_2d=False, dtype=None)
+        unique_groups = np.unique(groups)
+        if len(unique_groups) <= 1:
+            raise ValueError(
+                "The groups parameter contains fewer than 2 unique groups "
+                "(%s). LeaveOneGroupOut expects at least 2." % unique_groups
+            )
+        # self.groups_distribution : Dict[Int, List[Int]]
+        for i in range(self.n_splits):
+            fold_groups = []
+            for label, values in self.current_combination.items():
+                fold_groups.append(values[i])
+            test_mask = np.isin(groups, fold_groups)
+            yield test_mask
+
+    def split(self, X, y=None, groups=None):
+        return super().split(X, y, groups)
+
+    def get_n_splits(self, X=None, y=None, groups=None):
+        return self.n_splits
+
 
 class LeaveBalancedGroupsOut(GroupsConsumerMixin, BaseCrossValidator):
-
-    def __init__(self, shuffle : bool = False, random_state = None, used_combinations : set = None):
+    def __init__(self, shuffle: bool = False, random_state=None, used_combinations: set = None):
         if not isinstance(shuffle, bool):
             raise TypeError("shuffle must be True or False; got {0}".format(shuffle))
 
@@ -61,7 +124,7 @@ class LeaveBalancedGroupsOut(GroupsConsumerMixin, BaseCrossValidator):
         self.used_combinations = used_combinations if used_combinations is not None else set()
 
     def _compute_combinations(self, y, groups):
-        labels = np.unique(y)        
+        labels = np.unique(y)
         # Create a dictionary to map labels to their unique groups
         label_to_groups = {label: np.unique(groups[y == label]) for label in labels}
 
@@ -75,9 +138,7 @@ class LeaveBalancedGroupsOut(GroupsConsumerMixin, BaseCrossValidator):
         if groups is None:
             raise ValueError("The 'groups' parameter should not be None.")
         # We make a copy of groups to avoid side-effects during iteration
-        groups = check_array(
-            groups, input_name="groups", copy=True, ensure_2d=False, dtype=None
-        )
+        groups = check_array(groups, input_name="groups", copy=True, ensure_2d=False, dtype=None)
         unique_groups = np.unique(groups)
         if len(unique_groups) <= 1:
             raise ValueError(
@@ -86,7 +147,7 @@ class LeaveBalancedGroupsOut(GroupsConsumerMixin, BaseCrossValidator):
             )
 
         group_combinations = self._compute_combinations(y, groups)
-        # TODO: Improve loop and checking 
+        # TODO: Improve loop and checking
         folds_created = 0
         for combination in group_combinations:
             if combination not in self.used_combinations:
@@ -101,19 +162,22 @@ class LeaveBalancedGroupsOut(GroupsConsumerMixin, BaseCrossValidator):
             raise Exception("There are not enough combinations for the number of splits")
 
     def split(self, X, y=None, groups=None):
-        self.n_splits = len(np.unique(groups)) / len(np.unique(y))  # n_splits = total_groups / total_labels = num_conditions
+        self.n_splits = len(np.unique(groups)) / len(
+            np.unique(y)
+        )  # n_splits = total_groups / total_labels = num_conditions
         return super().split(X, y, groups)
 
     def get_n_splits(self, X=None, y=None, groups=None):
         return self.n_splits
-    
 
-from vibnet.data.group_dataset import GroupDataset
+
 from vibdata.deep.signal.core import SignalSample
 
-class GroupRepeatedCWRU48k(GroupDataset):
+from vibnet.data.group_dataset import GroupDataset
 
+
+class GroupRepeatedCWRU48k(GroupDataset):
     @staticmethod
     def _assigne_group(sample: SignalSample) -> int:
         sample_metainfo = sample["metainfo"]
-        return sample_metainfo['label'].astype(str) + " " + sample_metainfo['load'].astype(int).astype(str)
+        return sample_metainfo["label"].astype(str) + " " + sample_metainfo["load"].astype(int).astype(str)
